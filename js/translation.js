067caca618abd9b2e90d31582946782f0d7fa3b7
MentalNomad <34785888+MentalNomad@users.noreply.github.com>
2018-12-15 14:07:11 -0500

    Edits for clarity.
    Edited text on main page.
    
    Added reference on main page to mining process details blogpost, with fields for translation. (Included basic Google Translate of new fields.)

--- js/translation.js
+++ js/translation.js
@@ -1,10 +1,12 @@
 // Ready translated locale messages
 const translations = {
     en: {
-        title: 'BTG Transparency Page',
-        subtitle: 'aka. Have the team dumped the coins?',
-        total: 'Total',
+        title: 'BTG Endowment Transparency',
+        subtitle: 'All 100,000 coins mined November 12, 2017',
+        total: 'Live Unspent Total',
         loading: 'loading',
+		details: 'Full mining process ',
+		linktext: 'described here.',
         more_tools: 'More tools are coming soon.'
     },
     cn: {
@@ -12,6 +14,8 @@ const translations = {
         subtitle: '我倒是要看看团队有没有抛币砸盘。',
         total: '团队钱包总余额',
         loading: '载入中',
+        details: '采矿过程',
+        linktext: '这里描述的',
         more_tools: '更多工具敬请期待'
     }
 }
